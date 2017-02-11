11f6758b8efc54cc08b1a976ba0febaf07002df3
Angel <angelglezz.7@gmail.com>
2017-02-10 19:03:28 -0600

    responsive

---
+++ js/hope.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	$("#main").click(function(){
+		$("#main_item_content").show();
+	});
+
+	$("#erp_item, #first").click(function(){
+		var inner_alto = parseInt($(".inner").css('height'));
+		var top_bar = parseInt($("#top_bar").css('height'));
+		var bar_cont = parseInt($("#contact_content").css('height'));
+		var mov = inner_alto + top_bar + bar_cont;
+		$(document).scrollTop(mov);
+		$("#main_item_content").hide();
+	});
+
+	$("#aplicaciones, .apli").click(function(){
+		var inner_alto = parseInt($(".inner").css('height'));
+		var top_bar = parseInt($("#top_bar").css('height'));
+		var bar_cont = parseInt($("#contact_content").css('height'));
+		var job = parseInt($("#job").css('height'));
+		var job_margin = parseInt($("#job").css('margin-bottom'));
+		var mov = inner_alto + top_bar + bar_cont + job + job_margin;
+		$(document).scrollTop(mov);
+		$("#main_item_content").hide();
+	});
+
+	$("#experiencia").click(function(){
+		var inner_alto = parseInt($(".inner").css('height'));
+		var top_bar = parseInt($("#top_bar").css('height'));
+		var bar_cont = parseInt($("#contact_content").css('height'));
+		var job = parseInt($("#job").css('height'));
+		var job_margin = parseInt($("#job").css('margin-bottom'));
+		var app_margin = parseInt($("#apps").css('margin-bottom'));
+		var app = parseInt($("#apps").css('height'));
+		var mov = inner_alto + top_bar + bar_cont + job + job_margin + app + app_margin;
+		$(document).scrollTop(mov);
+		$("#main_item_content").hide();
+	});
+
+	$("#contactanos").click(function(){
+		var page = parseInt($("#page").css('height'));
+		$(document).scrollTop(page);
+		$("#main_item_content").hide();
+	});
+});
