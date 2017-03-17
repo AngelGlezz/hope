37d4b2ae61fcde53dec7fef855556cf594ddfdf4
Angel <angelglezz.7@gmail.com>
2017-03-17 12:21:42 -0600

    click sección trabajo

--- js/hope.js
+++ js/hope.js
@@ -52,4 +52,51 @@ $(document).ready(function(){
 	$("#main_content").mouseleave(function(){
 		$("#main_item_content").hide();
 	});
+
+	$("#erp").click(function(){
+		$("#two_content, .item_content, #title_job").hide();
+		$("#tipe, .fa").fadeIn(2000);
+		$("#title_tipe").text("ERP/CRM");
+		$("#intro_tipe").text("Desarrollo de programas hechos a la medida para el control y administración de recursos de la empresa, incluyendo sus procesos para mejorar las relaciones y ventas con los clientes.");
+		$("#complement_tipe").text("Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro. Todas estas borrascas que nos suceden son señales de que presto ha de serenar el tiempo y han de sucedernos bien las cosas; porque no es posible que el mal ni el bien sean durables, y de aquí se sigue que, habiendo durado mucho el mal, el bien está ya cerca. Así que, no debes congojarte por las desgracias que a mí me suceden, pues a ti no te cabe parte dellas.Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro. Todas estas borrascas que nos suceden son señales de que presto ha de serenar el tiempo y han de sucedernos bien las cosas; porque no es posible que el mal ni el bien sean durables, y de aquí se sigue que, habiendo durado mucho el mal, el bien está ya cerca. Así que, no debes congojarte por las desgracias que a mí me suceden, pues a ti no ");
+	});
+
+	$("#industria").click(function(){
+		$("#two_content, .item_content, #title_job").hide();
+		$("#tipe").fadeIn(2000);
+		$("#title_tipe").text("Industria de Fabricación y Transformación");
+		$("#intro_tipe").text("ERP que administra y controla todas las áreas: compras, cotizaciones y pedidos del cliente, almacén MP, órdenes de producción, empacado, embarques, almacén PT, facturación, cobranza, mantenimiento, ingeniería, sistema de gestión de calidad, control de documentos, costeos, presupuestos, reportes gerenciales, etc.");
+		$("#complement_tipe").text("Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro. Todas estas borrascas que nos suceden son señales de que presto ha de serenar el tiempo y han de sucedernos bien las cosas; porque no es posible que el mal ni el bien sean durables, y de aquí se sigue que, habiendo durado mucho el mal, el bien está ya cerca. Así que, no debes congojarte por las desgracias que a mí me suceden, pues a ti no te cabe parte dellas.Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro. Todas estas borrascas que nos suceden son señales de que presto ha de serenar el tiempo y han de sucedernos bien las cosas; porque no es posible que el mal ni el bien sean durables, y de aquí se sigue que, habiendo durado mucho el mal, el bien está ya cerca. Así que, no debes congojarte por las desgracias que a mí me suceden, pues a ti no ");
+	});
+
+	$("#valuaciones").click(function(){
+		$("#two_content, .item_content, #title_job").hide();
+		$("#tipe").fadeIn(2000);
+		$("#title_tipe").text("Valuaciones de Daños de Seguros");
+		$("#intro_tipe").text("Registro de casos, bitácoras, control de asesores técnicos, facturación electrónica, cobranza, tesorería, contabilidad, reportes varios, etc.");
+		$("#complement_tipe").text("Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro. Todas estas borrascas que nos suceden son señales de que presto ha de serenar el tiempo y han de sucedernos bien las cosas; porque no es posible que el mal ni el bien sean durables, y de aquí se sigue que, habiendo durado mucho el mal, el bien está ya cerca. Así que, no debes congojarte por las desgracias que a mí me suceden, pues a ti no te cabe parte dellas.Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro. Todas estas borrascas que nos suceden son señales de que presto ha de serenar el tiempo y han de sucedernos bien las cosas; porque no es posible que el mal ni el bien sean durables, y de aquí se sigue que, habiendo durado mucho el mal, el bien está ya cerca. Así que, no debes congojarte por las desgracias que a mí me suceden, pues a ti no ");
+	});
+
+	$("#back").click(function(){
+		$("#two_content, .item_content, #title_job").fadeIn(2000);
+		$("#tipe").hide();
+	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
